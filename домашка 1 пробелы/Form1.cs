e2b7d228e217c6ed35ee242dbad67c0ac45fb7cf
User <User@WIN-FHSUTN5TG5D>
2020-02-12 21:24:31 +0600

    Добавьте файлы проекта.

---
+++ домашка 1 пробелы/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace домашка_1_пробелы
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter textfile;
+            FileInfo file = new FileInfo("C:\\Users\\User\\Desktop\\1.txt");
+
+            textfile = file.AppendText();
+            textfile.WriteLine(textBox1.Text);
+            textfile.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader textfile = new StreamReader("C:\\Users\\User\\Desktop\\1.txt");
+            string str = "";
+
+            while (!textfile.EndOfStream)
+            {
+                str += textfile.ReadLine();
+            }
+            label1.Text = str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader stream = new StreamReader(openFileDialog1.FileName);
+
+                string str = "";
+
+                while (!stream.EndOfStream)
+                {
+                    str += stream.ReadLine();
+                }
+
+                foreach(char space in str)
+                {
+                    if(space==' ')
+                    {
+                        MessageBox.Show("ПРОБЕЛЕЛЕЛЕ");
+                    }
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
